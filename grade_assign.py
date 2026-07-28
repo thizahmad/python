667a552f80421afe4f051fa7208af2a282043f76
Mohammad Ahmad <123619899+thizahmad@users.noreply.github.com>
2026-07-28 16:12:32 +0530

    Add files via upload

---
+++ grade_assign.py
@@ -0,0 +1,13 @@
+marks = int(input("Enter your marks: "))
+
+
+if marks >= 90:
+    print("A")
+elif marks >= 80:
+    print("B")
+elif marks >= 70:
+    print("C")
+elif marks >= 60:
+    print("D")
+else:
+    print("F")
