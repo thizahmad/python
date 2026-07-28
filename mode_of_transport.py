4da3376d093c4d48148159abbcc700798d55fa6f
Mohammad Ahmad <123619899+thizahmad@users.noreply.github.com>
2026-07-28 16:26:59 +0530

    Add files via upload

---
+++ mode_of_transport.py
@@ -0,0 +1,10 @@
+distance = input("Enter the distance you wanna go: ")
+
+distance = int(distance)
+
+if distance < 3:
+    print("walk")
+elif distance < 15:
+    print("bring your dhoom machale")
+else:
+    print("take out your car")
---
+++ weather_activity.py
@@ -0,0 +1,10 @@
+weather = input("Enter your choice of whether: ")
+
+if weather == "sunny":
+    print("Go for a walk")
+elif weather == "rainy":
+    print("read a book")
+elif weather == "snowy":
+    print("build a snowman")
+else:
+    print("Chill and relax")
